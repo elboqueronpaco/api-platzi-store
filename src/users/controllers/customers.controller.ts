c90c143c4bd9d236cd386a88b514b1fe83fda67b
elboqueronpaco <elboqueronpaco@gmail.com>
2023-02-21 15:34:40 +0100

    service y controller customers

--- src/users/controllers/customers.controller.ts
+++ src/users/controllers/customers.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateCustomerDto } from '../dto/create-customer.dto';
+import { UpdateCustomerDto } from '../dto/update-customer.dto';
 import { CustomersService } from '../services/customers.service';
 
 @Controller('customers')
@@ -8,5 +21,28 @@ export class CustomersController {
   @HttpCode(HttpStatus.OK)
   async getCustomers() {
     return await this.customersService.findAll();
+  }
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.customersService.findOne(id);
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createCustomerDto: CreateCustomerDto) {
+    return await this.customersService.create(createCustomerDto);
+  }
+  @Put()
+  @HttpCode(HttpStatus.CREATED)
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
+    return await this.customersService.update(id, updateCustomerDto);
+  }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.customersService.remove(id);
   }
 }
--- src/users/services/customers.service.ts
+++ src/users/services/customers.service.ts
@@ -1,11 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { customers } from '../data/customers.data';
+import { CreateCustomerDto } from '../dto/create-customer.dto';
+import { UpdateCustomerDto } from '../dto/update-customer.dto';
 
 @Injectable()
 export class CustomersService {
   private counterId = 1;
   private customers = customers;
+  async getIndex(id: number) {
+    return this.customers.findIndex((item) => item.id === id);
+  }
   async findAll() {
     return this.customers;
+  }
+  async findOne(id: number) {
+    const customer = this.customers.find((item) => item.id === id);
+    if (!customer)
+      throw new HttpException(
+        `Customer #${id} NOT FOUND`,
+        HttpStatus.BAD_REQUEST,
+      );
+    return customer;
+  }
+  async create(creatreCustomerDto: CreateCustomerDto) {
+    this.counterId = this.counterId + 1;
+    const customer = {
+      id: this.counterId,
+      ...creatreCustomerDto,
+    };
+    this.customers.push(customer);
+    return customer;
+  }
+  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    const customer = await this.findOne(id);
+    const index = await this.getIndex(id);
+    this.customers[index] = {
+      ...customer,
+      ...updateCustomerDto,
+    };
+    return this.customers[index];
+  }
+  async remove(id: number) {
+    const index = await this.getIndex(id);
+    if (index === -1) throw new NotFoundException(`Customer #${id} NOT FOUND`);
+    this.customers.splice(index, 1);
+    return true;
   }
 }
